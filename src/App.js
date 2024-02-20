d6d33d5d14ec88338792810b4232c3eb99ac93fb
lilpotato03 <aaron.furtado20003@gmail.com>
2024-02-20 22:02:26 +0530

    mad required changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import {BrowserRouter,Routes,Route,NavLink} from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About'
 function App() {
   return (
-    <div className="App">
-      Hello, Ninjas!
-    </div>
+    <BrowserRouter>
+      <main>
+        <header>
+          <nav>
+            <h1>JobaRouter</h1>
+            <NavLink to='/'>Home</NavLink>
+            <NavLink to='/about'>About</NavLink>
+          </nav>
+        </header>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/about' element={<About />}/>
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
